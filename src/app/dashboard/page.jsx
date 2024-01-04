75dfef208f4ffecdd04783b2c31f86aebe07a8bd
Asha <asha.s@gndsolutions.in>
2024-01-04 17:59:51 +0530

    Changes

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import ContentSection from "../components/contentSection/contentsection";
 import HeroSection from "../components/heroSection/herosection";
 import DashboardLayout from './layout'
+import BlogSection from "../blogsection/page"
 
 const Dashboard = () => {
   return (
     <DashboardLayout>
       <div className="max-h-screen">
         <HeroSection />
+        <BlogSection/>
         <ContentSection />
       </div>
     </DashboardLayout>
--- src/app/login/form.jsx
+++ src/app/login/form.jsx
@@ -28,7 +28,6 @@ const Form = () => {
     setError(params.get("error"));
   }, [params]);
 
-
   const formSubmit = (form) => {
     const { email, password } = form;
     console.log(form, "form");
@@ -42,86 +41,89 @@ const Form = () => {
     router.push("/");
   }
   return (
-    <form
-      onSubmit={handleSubmit(formSubmit)}
-      className={` flex justify-center items-center flex-col`}
-    >
-      <h2
-        className={`leading-[1.15] mt-12 mx-auto w-full  px-2 text-xl my-6 sm:text-2xl font-semibold  font-Poppins`}
+    <form className="space-y-6" onSubmit={handleSubmit(formSubmit)}>
+    <div>
+      <label
+        htmlFor="email"
+        className="block text-sm font-medium leading-6 text-gray-900"
       >
-        Log In
-      </h2>
-
-      <fieldset className="w-full px-2 flex justify-center items-center flex-col">
-        <label className="w-full " htmlFor="email">
-          Email
-        </label>
+        Email
+      </label>
+      <div className="mt-2">
         <input
           type="email"
           {...register("email", {
             required: "Email is required",
             pattern: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
           })}
-          className=" w-full   border-solid border-[1px] border-[#EAECEF]"
+          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
         />
         {errors.email?.message && (
           <small className="block text-red-600 w-full">
             {errors.email.message}
           </small>
         )}
-      </fieldset>
-      <fieldset className="w-full px-2 mt-12 flex justify-center items-center flex-col">
-        <label className="w-full" htmlFor="password">
+      </div>
+    </div>
+  
+    <div>
+      <div className="flex items-center justify-between">
+        <label
+          htmlFor="password"
+          className="block text-sm font-medium leading-6 text-gray-900"
+        >
           Password
         </label>
+        <div className="text-sm">
+          <a
+            href="#"
+            className="font-semibold text-indigo-600 hover:text-indigo-500"
+          >
+            Forgot password?
+          </a>
+        </div>
+      </div>
+      <div className="mt-2">
         <input
           type="password"
           {...register("password", {
             required: "Password is required",
           })}
-          className=" w-full   border-solid border-[1px] border-[#EAECEF]"
+          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
         />
         {errors.password?.message && (
           <small className="block text-red-600 w-full">
             {errors.password.message}
           </small>
         )}
-      </fieldset>
-      <div className={`flex flex-col justify-center w-full items-center px-2`}>
-        <button
-          type="submit"
-          disabled={isSubmitting}
-          className="w-full flex justify-center items-center"
-        >
-          <span className="text-center flex-1   mt-6 bg-green-700 hover:bg-lightColor hover:font-semibold rounded-md p-[1rem] px-4  text-white cursor-pointer">
-            Log in
-          </span>
-        </button>
-        <p className={`py-6  text-[#707a8a] text-center`}>
-          <span className="mr-1 "> Or</span>
-        </p>
       </div>
-      <div className="flex w-full justify-center px-2 text-lg items-center">
-        <buuton>GoogleBtn</buuton>
-      </div>
-      {error && (
-        <small className="block w-full px-2 text-red-600">{error}</small>
-      )}
-
-      <div className="py-4 px-2 w-full">
-        <p>
-          <Link
-            href="/register"
-            className="text-lightColor hover:text-primaryColor hover:underline"
-          >
-            {" "}
-            Create an Account
-            <BsArrowRightCircleFill className="ml-2" />
-          </Link>
-        </p>
-      </div>
-      {isSubmitting && <h3>Loading...................</h3>}
-    </form>
+    </div>
+  
+    <div>
+      <button
+        type="submit"
+        disabled={isSubmitting}
+        className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+      >
+        Sign in
+      </button>
+    </div>
+  
+    {error && (
+      <small className="block text-red-600 w-full">{error}</small>
+    )}
+  
+    <p className="mt-10 text-center text-sm text-gray-500">
+      Not a member?{" "}
+      <a
+        href="#"
+        className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+      >
+        Start a 14 day free trial
+      </a>
+    </p>
+  </form>
+  
   );
 };
 
